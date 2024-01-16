ef3b12ed18b460d5adad334538972708abafa4dc
kashif123y007 <156598368+kashif123y007@users.noreply.github.com>
2024-01-16 03:41:56 -0800

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://parlifestyle.blogspot.com/"
+	const endpoint = "https://kashifyousaf.000webhostapp.com/graphql/"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
